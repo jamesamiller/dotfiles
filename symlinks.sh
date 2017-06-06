3a93b9b33a417e886ea8e8e25000f4e830dfdbba
James A Miller <millerja@uah.edu>
2017-06-06 10:48:31 -0500

    Added file
    symlinks.sh was added to Mathias Bynens’ dotfiles. I just it to establish sym links between my home directory and the local dotfiles repo.

---
+++ symlinks.sh
@@ -0,0 +1,18 @@
+#
+# Creates symlinks to maintained dotfiles.
+# Clone the dotfile repo to ~/GitHub/dotfiles first.
+#
+ln -s ~/GitHub/dotfiles/.aliases ~/.aliases
+ln -s ~/GitHub/dotfiles/.bash_profile ~/.bash_profile
+ln -s ~/GitHub/dotfiles/.bash_prompt ~/.bash_prompt
+ln -s ~/GitHub/dotfiles/.exports ~/.exports
+ln -s ~/GitHub/dotfiles/.functions ~/.functions
+ln -s ~/GitHub/dotfiles/.path ~/.path
+ln -s ~/GitHub/dotfiles/.vim ~/.vim
+ln -s ~/GitHub/dotfiles/.vimrc ~/.vimrc
+ln -s ~/GitHub/dotfiles/.gvimrc ~/.gvimrc
+ln -s ~/GitHub/dotfiles/.extra ~/.extra
+#
+# Put Sublime Text in the path
+ln -s "/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl" /usr/local/bin/subl
+
